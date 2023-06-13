2e8f60000c466afd5551d64e0295f83f6f64a5cc
lokeshwardeb <lokeshwardebdevelop@gmail.com>
2023-06-13 14:40:29 +0600

    initializing the first commit of the project

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $website_name ?> || Banking system</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <main>
+    Welcome to the banking system
+
+    </main>
+
+
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/script.js"></script>
+
+</body>
+</html>
